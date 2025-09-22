6824b676b5b4ee86a1079eba1113db307bb5ee24
Sudarshan Gorakhnath Phule <sgphule@gmail.com>
2025-09-22 04:22:18 +0200

    CI test 1 after implementing model and application tests

---
+++ tests/test_application.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from flask_app.application import app
+
+class Test(TestCase):
+    def test_predict(self):
+        self.fail()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = app.test_client()
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<title>Sentiment Analysis</title>', response.data)
+
+    def test_predict_page(self):
+        response = self.client.post('/predict', data=dict(text="First half was excellent but the end was too bad!"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            b'Positive' in response.data or b'Negative' in response.data,
+            "Response should contain either 'Positive' or 'Negative'"
+        )
